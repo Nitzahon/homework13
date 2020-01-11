88e100141412745a9452d4ddf4929b547ffa3b41
Nitzahon <nadavga@gmail.com>
2020-01-11 16:58:48 +0200

    added empty cart, populated home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import home from "./home.svg";
+import supermarket from "./supermarket.svg";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Cart from './components/Cart'
+import Home from './components/Home.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    flag: false,
+    products: [
+      {
+        name:'Pre-made Desktop',
+        price:100
+      },
+      {
+        name:'Paper',
+        price:1
+      },
+      {
+        name:'Pen',
+        price:10
+      },
+      {
+        name:'Disk',
+        price:5
+      }
+    ],
+    Cart: [
+      {
+
+      }
+    ],
+    buy: []
+  };
+atc=()=>{
+  this.setState({Cart:[]});
 }
+  show = () => {
+    if (this.state.flag == true) {
+      return (
+        <div>
+          {/* <Cart /> */}
+          true
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div>
+          {this.state.products.map((element)=>{
+        return <Home name={element.name} price={element.price} addtoCart={this.atc}/>
+      })}
+          false
+        </div>
+        <div>
 
-export default App;
+        </div>
+        </div>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-1">
+            <button
+              onClick={() => {
+                this.setState({ flag: false });
+              }}
+            >
+              <img src={home} className="App-logo" alt="homepage" />
+            </button>
+          </div>
+          <div className="col-10"><p className="titleCard">SV-SHOP</p></div>
+          <div className="col-1">
+            <button
+              onClick={() => {
+                this.setState({ flag: true });
+              }}
+            >
+              <img src={supermarket} className="App-logo" alt="cart" />
+            </button>
+          </div>
+        </div>
+
+        {this.show()}
+      </div>
+    );
+  }
+}
---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state={
+                index:props.index,
+                name:props.name,
+                price:props.price
+        }
+      }
+    
+    render() {
+        return (
+            
+<div className="row">
+                <div className="col-1"></div>
+                <div className="col-6 row blueRow">
+        <p className="col-3 prodName">{this.state.name}</p>
+        <p className="col-1">=</p>
+                    <div className="col-3"></div>
+                    <p className="col-2 prodPrice">{this.state.price}&#x20aa;</p>
+                    <div className="col-1"></div>
+                    <button className="col-1 btn-info">+</button>
+                </div>
+
+                <div className="col-5"></div>
+</div>
+        )
+    }
+}
